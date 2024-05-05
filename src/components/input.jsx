ff18aa9db4ffd2bc3d94f31e5b311bb25c5862a9
daviddozie <davidmgbede75@gmail.com>
2024-05-05 07:12:55 -0700

    input

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,7 +1,20 @@
-function input() {
-  return (
-    <></>
-  )
+function Input({ inputType, inputName, placeholder, onChange, labelFor, label, inputClass, labelClass }) {
+    return (
+        <>
+            <div>
+                <label htmlFor={labelFor} className={`text-[14px] font-[500] text-[#E7E7E7] block ${labelClass}`}>
+                    {label}
+                </label>
+                <input
+                    type={inputType}
+                    name={inputName}
+                    placeholder={placeholder}
+                    onChange={onChange}
+                    className={`bg-[transparent] px-3 py-4 ${inputClass} `}
+                />
+            </div>
+        </>
+    )
 }
 
-export default input
+export default Input
