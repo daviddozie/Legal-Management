0cc3383e25cb3dc478f1f1a064f4c82833b04180
daviddozie <davidmgbede75@gmail.com>
2024-05-05 06:30:10 -0700

    develop

--- src/pages/LoginPage/Login.jsx
+++ src/pages/LoginPage/Login.jsx
@@ -1,9 +1,7 @@
 function Login() {
     return (
         <div className="lightgray h-full w-full">
-            <div className="flex justify-center pt-[40px]">
-                <h1 className="text-[#E6E6E6] text-[30px] font-[500]">Log in</h1>
-            </div>
+            <h1 className="text-[#E6E6E6] text-[30px] font-[500]">Log in</h1>
         </div>
     )
 }
