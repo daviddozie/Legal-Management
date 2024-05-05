5fdad96ff251341d3b1d9ebf684bd6cc379dab00
jesam33 <obasesamgospel@gmail.com>
2024-05-05 14:41:32 +0100

    h3 -login

--- src/pages/LoginPage/Login.jsx
+++ src/pages/LoginPage/Login.jsx
@@ -1,9 +1,12 @@
 function Login() {
-    return (
-        <div className="lightgray h-full w-full">
-            <h1 className="text-[#E6E6E6] text-[30px] font-[500]">Log in</h1>
-        </div>
-    )
+  return (
+    <div className="lightgray h-full w-full">
+      <h1 className="text-[#E6E6E6] text-[30px] font-[500]">Log in</h1>
+      <h3>
+        Don't have an account? <span>Sign Up</span>
+      </h3>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
